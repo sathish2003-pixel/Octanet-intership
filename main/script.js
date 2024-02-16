971a0cfeeffa2751b9c119a8bb6207a63c1151da
sathish2003-pixel <satheshsathish2206@gmail.com>
2024-02-16 16:37:35 +0530

    added files

---
+++ main/script.js
@@ -0,0 +1,5 @@
+function tooglemode(){
+    document.body.classList.toggle('dark-mode');
+    let button=document.getElementById("btn1");
+    document.getElementById("btn1").value="Light mode";
+  }
